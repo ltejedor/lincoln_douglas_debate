2ba3baeed47a8d0cecc66ca8833bc715b5af6b21
Leandra Tejedor <lmtejedor@gmail.com>
2013-08-26 09:53:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lincolndouglas::Application.config.secret_token = '84fafcc24430c7fb6a0d533fe9d1970c94ff885948c34eb8b409fe35af9bc21204c710e3d6c3c06334ed9c0f9716b628702d09d959c747702a7f79bceb72a379'
