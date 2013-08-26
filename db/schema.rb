33e69e6f8efb8d89bea848644966874ea537a015
Leandra Tejedor <lmtejedor@gmail.com>
2013-08-26 14:26:33 -0700

    show nested cards

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(:version => 20130826200301) do
 
   add_index "cards", ["topic_id"], :name => "index_cards_on_topic_id"
 
+  create_table "pages", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "topics", :force => true do |t|
     t.string   "name"
     t.string   "description"
---
+++ spec/support/factories/card_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+	factory :card do
+		title "Evidencexample"
+		description "Lincoln the vampire hunter was sub par"
+	end
+end
