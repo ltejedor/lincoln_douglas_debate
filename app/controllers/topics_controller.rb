65adcfb429d4b4cb25fb399193b2e41806abb288
Leandra Tejedor <lmtejedor@gmail.com>
2013-08-26 10:41:48 -0700

    begin to set up tests and page to upload evidence cards

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,4 +4,22 @@ class TopicsController < ApplicationController
 
 	end
 
+	def new
+		@topic = Topic.new
+	end
+
+	def create
+		@topic = Topic.new(params[:topic])
+		if @topic.save
+			flash[:notice] = "Debate Topic has been added."
+			redirect_to @topic
+		else
+# nothing, yet
+		end
+	end
+
+	def show
+		@topic = Topic.find(params[:id])
+	end
+
 end
