a2f560883421bec9a0174a15d09c0a6831a0cf26
Amy Hua <foramyhua@gmail.com>
2013-08-27 01:34:32 -0700

    Added seed tourney data and fixed footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tournament.create!(
+  name: 'Tournament of the Ages',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image: 'http://compassiondave.files.wordpress.com/2010/10/pewter_trophy_lrg.jpg'
+)
