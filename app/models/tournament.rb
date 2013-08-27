dad6b7aef1d30319af647bbc6f19581455ede745
Amy Hua <foramyhua@gmail.com>
2013-08-27 16:06:08 -0700

    Added launchy gem, tournament creation validation and passed tournament creation specs

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -11,7 +11,9 @@ class Tournament < ActiveRecord::Base
   validates_length_of :description, :maximum => 500, 
       :message => "Description should be less than 500 characters."
   
-  validates :start, :end, :name, :description, :presence => true
+  validates :name, :description, :presence => true
+  
+  validate :ends_after_starting?
   
   # TODO: Set time zone settings in Account Settings
   
@@ -25,4 +27,9 @@ class Tournament < ActiveRecord::Base
     self.end.strftime("%a %b %d, %Y %I:%M %p") if self.end
   end
   
+  def ends_after_starting?
+    if self.start > self.end || ((self.end - self.start) / 60) < 5
+      errors.add(:start, "your tournament reasonably earlier than it ends")
+    end 
+  end
 end
