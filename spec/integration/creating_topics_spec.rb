3cf177cae085bbd4f5a5013f7c560898565a42bf
Leandra Tejedor <lmtejedor@gmail.com>
2013-08-29 16:37:11 -0700

    Add starring card functionality

--- spec/integration/creating_topics_spec.rb
+++ spec/integration/creating_topics_spec.rb
@@ -9,12 +9,12 @@ feature 'Creating Topics' do
 	scenario 'can create a topic' do
 		fill_in 'Body', :with => 'Should we start a question club?'
 		click_button "Create Card"
-		page.should have_content('Debate Topic has been added.')
+		page.should have_content('Card has been added.')
 	end
 
 	scenario "can not create a project without a name" do
 		click_button 'Create Card'
-		page.should have_content("Debate Topic has not been added.")
+		page.should have_content("Card has not been added.")
 		page.should have_content("Body can't be blank")
 	end
 end
