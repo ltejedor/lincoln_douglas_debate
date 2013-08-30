4bab21a4aec659e7159be3f8ba5f231ef566234d
Leandra Tejedor <lmtejedor@gmail.com>
2013-08-29 21:42:36 -0700

    Set production for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Lincolndouglas::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+
+  ENV['GOOGLE_SECRET'] = "Q1qfMIJFrFbwIthTFUeZW9aw"
+  ENV['GOOGLE_KEY'] = "593301061123.apps.googleusercontent.com"
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
