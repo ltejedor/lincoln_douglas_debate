6af0007051e67e482551bf1afca79baccdd54eae
Amy Hua <foramyhua@gmail.com>
2013-08-30 05:28:46 -0700

    Organizer Dashboard styling and bracket setup

--- app/models/tournament_parts/round.rb
+++ app/models/tournament_parts/round.rb
@@ -1,8 +1,9 @@
 class Round < ActiveRecord::Base
   attr_accessible :kind #unpowered prelim, powered prelim, octofinal, quarterfinal, semifinal, final
-  attr_accessible :order  
+  attr_accessible :order, :brackets_attributes  
   belongs_to :tournament  
   has_many :brackets, :dependent => :delete_all
+  accepts_nested_attributes_for :brackets, allow_destroy: true
   
   # TODO: be able to add a round out of order and place in order.
   
