6d0d5c120d2bcdc93de85147342f4ed4c27fc7aa
Amy Hua <foramyhua@gmail.com>
2013-09-04 12:16:14 -0700

    Moved javascript files and deleted require tree for heroku deployment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-
+//= require forms
+//= require gplus
 //= require twitter/bootstrap
 
 
@@ -49,5 +49,100 @@ $('.achievement').tooltip()
 // TODO: Prevent Navbar overlap on div below.
 // (Navbar currently overlaps content while changing in size)
 // Use jQuery solution: http://stackoverflow.com/questions/11920846/prevent-a-div-to-overlap-over-a-fixed-one
+
+// ******* FORMS *********
+
+
+$('form').on('click', '.remove_fields', function(event) {
+  $(this).prev('input[type=hidden]').val('1');
+  $(this).closest('fieldset').hide();
+  return event.preventDefault();
+});
+
+$('form').on('click', '.remove_fields_division', function(event) {
+  $(this).next().next().next().removeClass('hide-this');
+  return event.preventDefault();
+});
+
+$('form').on('click', '.remove_fields_brackets', function(event) {
+  $(this).prev('input[type=hidden]').val('1');
+  $(this).parent().parent().hide();
+  return event.preventDefault();
+});
+
+$('form').on('click', '.remove_fields_in_table', function(event) {
+  $(this).prev('input[type=hidden]').val('1');
+  $(this).parent().parent().closest('fieldset').hide();
+  return event.preventDefault();
+});
+
+$('form').on('click', '.add_fields', function(event) {
+  var regexp, time;
+  time = new Date().getTime();
+  regexp = new RegExp($(this).data('id'), 'g');
+  $(this).before($(this).data('fields').replace(regexp, time).replace(/dynamic_time/g, time));
+  return event.preventDefault();
+});
+
+$('form').on('click', '.add_fields_in_table', function(event) {
+  var num, regexp, time, uniqueID;
+  time = new Date().getTime();
+  num = $(this).last().parent().parent().index();
+  uniqueID = time + '-' + num;
+  regexp = new RegExp($(this).data('id'), 'g');
+  $(this).parent().parent().before($(this).data('fields').replace(regexp, uniqueID).replace(/dynamic_time/g, uniqueID));
+  return event.preventDefault();
+});
+
+$('form').on('click', '.round-row', function(event) {
+  var time;
+  time = new Date().getTime();
+  $(this).attr('data-target', '#round-' + time);
+  return $(this).next().attr('id', 'round-' + time);
+});
+
+$('form').on('change', '.round-kind', function(event) {
+  return $(this).parent().parent().parent().parent().prev().children().first().next().removeClass('bracket-filler').html('<b>' + $(this).val() + '</b>');
+});
+
+$('form').on('click.current', '.add_fields_in_row', function(event) {
+  var regexp, time;
+  time = new Date().getTime();
+  regexp = new RegExp($(this).data('id'), 'g');
+  $(this).parent().before($(this).data('fields').replace(regexp, time).replace(/dynamic_time/g, time));
+  return event.preventDefault();
+});
+
+$("form").on("keyup", "input.bracket-time", function() {
+  var re;
+  re = /\d{1,2}\/\d{1,2}\/\d{4} \d{1,2}:\d{2}(am|AM|pm|PM)/;
+  if ($(this).val().match(re)) {
+    return $(this).parent().parent().next().addClass("hide-this");
+  } else {
+    return $(this).parent().parent().next().removeClass("hide-this");
+  }
+});
+
+$('form').on('click', '.new-division-link', function() {
+  if ($(this).parent().parent().next().children().children('.row').children('.division-name').children('.tournament_divisions_name').children('div').children().val() === "") {
+    return $(this).parent().parent().next().children().children('table').addClass('hide-table');
+  }
+});
+
+$("form").on("keyup", "input[placeholder=\"Division Name\"]", function() {
+  if ($(this).val() !== "") {
+    $(this).first().parent().parent().parent().parent().next().next().removeClass("hide-table");
+  }
+  if ($(this).val() === "") {
+    $(this).first().parent().parent().parent().parent().next().next().addClass("hide-table");
+  }
+  if ($(this).val() === "") {
+    $(this).last().parent().parent().parent().parent().parent().parent().prev().children('h4').children('a').text('Unnamed Division (will be deleted)');
+  }
+  if ($(this).val() !== "") {
+    return $(this).last().parent().parent().parent().parent().parent().parent().prev().children('h4').children('a').text($(this).val());
+  }
+});
+
 
 
