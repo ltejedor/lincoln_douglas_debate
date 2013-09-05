bbfe10b6593c9c1400c671de880e0459a182307c
Amy Hua <foramyhua@gmail.com>
2013-09-05 10:14:31 -0700

    Navbar fixes and Comments in Application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,14 +38,14 @@ $(document).ready(function() {
 
 // Nested Forms
 
-function remove_fields(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
-}
+    function remove_fields(link) {
+      $(link).previous("input[type=hidden]").value = "1";
+      $(link).up(".fields").hide();
+    }
 
 // Tooltips
-$('.achievement').tooltip()
-$('.connection').tooltip()
+    $('.achievement').tooltip()
+    $('.connection').tooltip()
 
 // TODO: Prevent Navbar overlap on div below.
 // (Navbar currently overlaps content while changing in size)
@@ -54,96 +54,106 @@ $('.connection').tooltip()
 // ******* FORMS *********
 
 
-$('form').on('click', '.remove_fields', function(event) {
-  $(this).prev('input[type=hidden]').val('1');
-  $(this).closest('fieldset').hide();
-  return event.preventDefault();
-});
+    // Remove fields
+    $('form').on('click', '.remove_fields', function(event) {
+      $(this).prev('input[type=hidden]').val('1');
+      $(this).closest('fieldset').hide();
+      return event.preventDefault();
+    });
 
-$('form').on('click', '.remove_fields_division', function(event) {
-  $(this).next().next().next().removeClass('hide-this');
-  return event.preventDefault();
-});
 
-$('form').on('click', '.remove_fields_brackets', function(event) {
-  $(this).prev('input[type=hidden]').val('1');
-  $(this).parent().parent().hide();
-  return event.preventDefault();
-});
+    // - Division
+    $('form').on('click', '.remove_fields_division', function(event) {
+      $(this).next().next().next().removeClass('hide-this');
+      return event.preventDefault();
+    });
 
-$('form').on('click', '.remove_fields_in_table', function(event) {
-  $(this).prev('input[type=hidden]').val('1');
-  $(this).parent().parent().closest('fieldset').hide();
-  return event.preventDefault();
-});
+    // - Bracket
+    $('form').on('click', '.remove_fields_brackets', function(event) {
+      $(this).prev('input[type=hidden]').val('1');
+      $(this).parent().parent().hide();
+      return event.preventDefault();
+    });
 
-$('form').on('click', '.add_fields', function(event) {
-  var regexp, time;
-  time = new Date().getTime();
-  regexp = new RegExp($(this).data('id'), 'g');
-  $(this).before($(this).data('fields').replace(regexp, time).replace(/dynamic_time/g, time));
-  return event.preventDefault();
-});
+    $('form').on('click', '.remove_fields_in_table', function(event) {
+      $(this).prev('input[type=hidden]').val('1');
+      $(this).parent().parent().closest('fieldset').hide();
+      return event.preventDefault();
+    });
 
-$('form').on('click', '.add_fields_in_table', function(event) {
-  var num, regexp, time, uniqueID;
-  time = new Date().getTime();
-  num = $(this).last().parent().parent().index();
-  uniqueID = time + '-' + num;
-  regexp = new RegExp($(this).data('id'), 'g');
-  $(this).parent().parent().before($(this).data('fields').replace(regexp, uniqueID).replace(/dynamic_time/g, uniqueID));
-  return event.preventDefault();
-});
+    // + Division
+    $('form').on('click', '.add_fields', function(event) {
+      var regexp, time;
+      time = new Date().getTime();
+      regexp = new RegExp($(this).data('id'), 'g');
+      $(this).before($(this).data('fields').replace(regexp, time).replace(/dynamic_time/g, time));
+      return event.preventDefault();
+    });
 
-$('form').on('click', '.round-row', function(event) {
-  var time;
-  time = new Date().getTime();
-  $(this).attr('data-target', '#round-' + time);
-  return $(this).next().attr('id', 'round-' + time);
-});
+    // + Round
+    $('form').on('click', '.add_fields_in_table', function(event) {
+      var num, regexp, time, uniqueID;
+      time = new Date().getTime();
+      num = $(this).last().parent().parent().index();
+      uniqueID = time + '-' + num;
+      regexp = new RegExp($(this).data('id'), 'g');
+      $(this).parent().parent().before($(this).data('fields').replace(regexp, uniqueID).replace(/dynamic_time/g, uniqueID));
+      return event.preventDefault();
+    });
 
-$('form').on('change', '.round-kind', function(event) {
-  return $(this).parent().parent().parent().parent().prev().children().first().next().removeClass('bracket-filler').html('<b>' + $(this).val() + '</b>');
-});
+    // Round collapse, unique ID fix for +Round in New Divisions
+    $('form').on('click', '.round-row', function(event) {
+      var time;
+      time = new Date().getTime();
+      $(this).attr('data-target', '#round-' + time);
+      return $(this).next().attr('id', 'round-' + time);
+    });
 
-$('form').on('click.current', '.add_fields_in_row', function(event) {
-  var regexp, time;
-  time = new Date().getTime();
-  regexp = new RegExp($(this).data('id'), 'g');
-  $(this).parent().before($(this).data('fields').replace(regexp, time).replace(/dynamic_time/g, time));
-  return event.preventDefault();
-});
+    // Round editing live-update
+    $('form').on('change', '.round-kind', function(event) {
+      return $(this).parent().parent().parent().parent().prev().children().first().next().removeClass('bracket-filler').html('<b>' + $(this).val() + '</b>');
+    });
 
-$("form").on("keyup", "input.bracket-time", function() {
-  var re;
-  re = /\d{1,2}\/\d{1,2}\/\d{4} \d{1,2}:\d{2}(am|AM|pm|PM)/;
-  if ($(this).val().match(re)) {
-    return $(this).parent().parent().next().addClass("hide-this");
-  } else {
-    return $(this).parent().parent().next().removeClass("hide-this");
-  }
-});
+    // + Bracket
+    $('form').on('click.current', '.add_fields_in_row', function(event) {
+      var regexp, time;
+      time = new Date().getTime();
+      regexp = new RegExp($(this).data('id'), 'g');
+      $(this).parent().before($(this).data('fields').replace(regexp, time).replace(/dynamic_time/g, time));
+      return event.preventDefault();
+    });
 
-$('form').on('click', '.new-division-link', function() {
-  if ($(this).parent().parent().next().children().children('.row').children('.division-name').children('.tournament_divisions_name').children('div').children().val() === "") {
-    return $(this).parent().parent().next().children().children('table').addClass('hide-table');
-  }
-});
+    // Bracket time check valid datetime format
+    $("form").on("keyup", "input.bracket-time", function() {
+      var re;
+      re = /\d{1,2}\/\d{1,2}\/\d{4} \d{1,2}:\d{2}(am|AM|pm|PM)/;
+      if ($(this).val().match(re)) {
+        return $(this).parent().parent().next().addClass("hide-this");
+      } else {
+        return $(this).parent().parent().next().removeClass("hide-this");
+      }
+    });
 
-$("form").on("keyup", "input[placeholder=\"Division Name\"]", function() {
-  if ($(this).val() !== "") {
-    $(this).first().parent().parent().parent().parent().next().next().removeClass("hide-table");
-  }
-  if ($(this).val() === "") {
-    $(this).first().parent().parent().parent().parent().next().next().addClass("hide-table");
-  }
-  if ($(this).val() === "") {
-    $(this).last().parent().parent().parent().parent().parent().parent().prev().children('h4').children('a').text('Unnamed Division (will be deleted)');
-  }
-  if ($(this).val() !== "") {
-    return $(this).last().parent().parent().parent().parent().parent().parent().prev().children('h4').children('a').text($(this).val());
-  }
-});
+    // Hiding/ Showing +Round table based on Division Name blankness
+
+    $('form').on('click', '.new-division-link', function() {
+      if ($(this).parent().parent().next().children().children('.row').children('.division-name').children('.tournament_divisions_name').children('div').children().val() === "") {
+        return $(this).parent().parent().next().children().children('table').addClass('hide-table');
+      }
+    });
+
+    $("form").on("keyup", "input[placeholder=\"Division Name\"]", function() {
+      // Not blank: unhide Rounds table, update division name in Header
+      if ($(this).val() !== "") {
+        $(this).first().parent().parent().parent().parent().next().next().removeClass("hide-table");
+        $(this).last().parent().parent().parent().parent().parent().parent().prev().children('h4').children('a').text($(this).val());
+      }
+      // Blank: hide Rounds table, name Header 'Unnamed Division (will be deleted)'
+      if ($(this).val() === "") {
+        $(this).first().parent().parent().parent().parent().next().next().addClass("hide-table");
+        $(this).last().parent().parent().parent().parent().parent().parent().prev().children('h4').children('a').text('Unnamed Division (will be deleted)');
+      }
+    });
 
 
 
